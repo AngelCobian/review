89b05f335be49e5c9daf08480d9a2a4bffa26a36
unknown <angelcobian@outlook.com>
2018-06-07 16:33:36 -0700

    Types of typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  typeA: null  = null
+  typeB: undefined = undefined;
+  typeC: number = 12;
+  typeD: string = 'tiger';
+  typeE: boolean = true;
+  typeArray: any[] = [true, 12, 'tiger', null, undefined];
+  ngOnInit() {
+    console.log(this.typeArray);
+  }
+
 }
+
