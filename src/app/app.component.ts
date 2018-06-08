3c6ef806edaa8c52b07bb7dec9d307eee54ce594
unknown <angelcobian@outlook.com>
2018-06-08 02:04:35 -0700

    Fixing, because I was playing with the code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { StaticInjector } from '@angular/core/src/di/injector';
 
  class CarBrand {
-  private make: string;
+  protected make: string;
   constructor(theMake: string) {
     this.make = theMake;
   }
 }
-class Honda extends CarBrand {
+class Car extends CarBrand {
 
-  // private year: number;
-  // protected model: string;
-  constructor(protected year: number, make: string, private model: string) {
+  private year: number;
+  protected model: string;
+  constructor(year: number, make: string, model: string) {
     super(make);
-    // (code ide)TS is saying that 'make' can't be used becuse of it being private but, for some reason its still being console log. Is it in the wrong place?
-    console.log(this.make);
-    console.log(this.year);
-    console.log(this.model);
-    // this.year = year;
-    // this.model = model;
+    this.year = year;
+    this.model = model;
+    console.log('make :'+ this.make);
+    console.log('year:' +  this.year);
+    console.log("model:" + this.model);
 
   }
   getDetails() {
-    return `their are ${this.year} ${this.make} and their ${this.model} `;
+    return `This is an exmpale ${this.year} ${this.make} ${this.model}. `;
   }
 }
 
@@ -34,13 +33,13 @@ class Honda extends CarBrand {
 export class AppComponent implements OnInit {
   title = 'app';
   show: any = this.theseCars();
-  // ford: CarBrand = new CarBrand ('Ford');
+  ford: CarBrand = new CarBrand ('Ford');
   ngOnInit() {
     // this.theseCars();
 
   }
   theseCars() {
-    const f = new Honda(2, 'Dogs', 'not a car');
+    const f = new Car(1999, 'Handa', 'ts-130');
     console.log(f.getDetails());
     return f.getDetails();
   }
